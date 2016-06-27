b199bfffd1a26ea2d789d3d595d52169e8970428
Simon Otter <simonotter@gmail.com>
2016-06-27 12:01:41 +0100

    add name and role via JavaScript string replacements and use JQuery to append to HTML template

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var name = "Simon Otter";
+var role = "Developer"
+
+var formattedName = HTMLheaderName.replace("%data%", name)
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").append(formattedName)
+$("#header").append(formattedRole)
